7f62a8356f89b92f80c6a563f23a602487102677
akhras018 <akhras018@gmail.com>
2025-12-19 15:40:16 +0200

    aaaa

---
+++ pro-1/Models/aaaar.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace pro_1.Models
+{
+    public class aaaar : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
