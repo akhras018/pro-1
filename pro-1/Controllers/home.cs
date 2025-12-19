08bbdec284e9b93c7e14747efd3e262c37a0b264
ismaelbadran <IsmaeBa@ac.sce.ac.il>
2025-12-19 15:38:37 +0200

    .

---
+++ pro-1/Controllers/home.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace pro_1.Controllers
+{
+    public class home : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
